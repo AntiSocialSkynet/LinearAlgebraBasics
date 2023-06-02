6bfde4b87e40cbabfc2e81473346d9fceafc660d
Andrew Bauman <andrew.bauman332@gmail.com>
2023-06-02 08:07:24 -0500

    Add project files.

---
+++ GaussianElimination.cs
@@ -0,0 +1,13 @@
+﻿namespace LinearAlgebraBasics
+{
+    /// <summary>
+    /// Class equipped with methods to handle the operation of Gaussian ELimination on an arbitrary n x m matrix.
+    /// </summary>
+    public class GaussianElimination
+    {
+        public static int[,] RowInterchange(int[,] matrix,int row1, int row2)
+        {
+            int[] Temp = new int[matrix.GetLength(1)];//The row length should equal the number of columns in the matrix.
+        } 
+    }
+}
