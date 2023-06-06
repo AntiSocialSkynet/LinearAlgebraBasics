41587fa251ac6d8762946a218e20787b1c35f273
Andrew Bauman <andrew.bauman332@gmail.com>
2023-06-06 04:40:46 -0500

    Added Inverse Matrix Implementation. Added some comments.

--- Matrix.cs
+++ Matrix.cs
@@ -8,7 +8,7 @@ using System.Threading.Tasks;
 namespace LinearAlgebraBasics
 {
     /// <summary>
-    /// Class which provides basic methods for matrix generation.
+    /// Class which provides basic methods for matrix generation and matrix algebra.
     /// </summary>
    public class Matrix
     {
@@ -205,6 +205,23 @@ namespace LinearAlgebraBasics
             return expMatrix;
         }
 
+        public static float[,] Inverse(float[,] matrix)
+        {
+            //we splice the n x n identity matrix onto the given matrix.
+            float[,] matrixAugment = Matrix.AugmentMatrix(matrix, Matrix.IdentityMatrix(matrix.GetLength(0)));
+            GaussianElimination.ReducedEchelonForm(matrixAugment); //reduce to the reduced echelon form of the given matrix
+            Matrix.PrintMatrix(matrixAugment);
+            //we now need to split off the identity matrix.
+            float[,] inverseMatrix = new float[matrix.GetLength(0), matrix.GetLength(1)];//these dimensions *should* be the same
 
+            for(int i  = 0; i < matrix.GetLength(0); i++)
+            {
+                for(int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    inverseMatrix[i, j] = matrixAugment[i, j + matrix.GetLength(1)];
+                }
+            }
+            return inverseMatrix;
+        }
     }
 }
